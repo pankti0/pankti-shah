74e97f944dadc6a5db0c638e132caa1494c66498
pankti0 <89905861+pankti0@users.noreply.github.com>
2024-05-21 16:02:40 +0800

    NavBar done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+
+
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-white selection:text-black">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="container mx-auto px-8">
+      <NavBar />
+      <Hero/>
+      
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
